7d87fe07eca64c4d2733b2240013679ace305edb
Rafał Żmuda <36961066+RafalZmu@users.noreply.github.com>
2024-03-14 19:29:57 +0100

    Initial commit with working map

---
+++ MapCompare/src/main.ts
@@ -0,0 +1,7 @@
+import './assets/main.css'
+import '@coddicat/vue-pinch-scroll-zoom/style.css';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
---
+++ MapCompereAPI/MapCompereAPI/Controllers/MapController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MapCompereAPI.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class MapController : ControllerBase
+	{
+		private string _mapFilePath = "./Assets/world - Copy.svg";
+
+		[HttpGet]
+		public IActionResult GetMap()
+		{
+			if (System.IO.File.Exists(_mapFilePath))
+			{
+				// Read the SVG file content
+				string svgContent = System.IO.File.ReadAllText(_mapFilePath);
+
+				// Set the response content type
+				HttpContext.Response.ContentType = "image/svg+xml";
+
+				// Write the SVG content to the response output stream
+				return Content(svgContent);
+			}
+			else
+			{
+				// File not found, return a 404 status code or handle it accordingly
+				return NotFound();
+			}
+		}
+	}
+}
