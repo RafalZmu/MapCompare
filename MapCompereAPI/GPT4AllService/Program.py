79b95e548db895d2a83bee3277120dcb6e629752
RafalZmu <rafalzmu15@gmial.com>
2024-06-20 23:38:29 +0200

    Update modelPath in Program.py

--- MapCompereAPI/GPT4AllService/Program.py
+++ MapCompereAPI/GPT4AllService/Program.py
@@ -1,7 +1,7 @@
 from gpt4all import GPT4All
 
 modelName = "mistral-7b-instruct-v0.1.Q4_0.gguf"
-modelPath = f"models/{modelName}"
+modelPath = "D:\Projekty\Praca_inz\MapCompare\MapCompereAPI\GPT4AllService\Models"
 
 model = GPT4All(model_name=modelName, model_path=modelPath, device="gpu")
 
